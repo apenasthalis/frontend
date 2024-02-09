f5e3dfca8cc15a651c55b5208fa06bf465b851fc
apenasthalis <1thalisgabriel1@gmail.com>
2024-02-09 15:47:36 -0300

    enviando a foto pro banco

--- src/components/pages/RegistroFotografico.jsx
+++ src/components/pages/RegistroFotografico.jsx
@@ -1,5 +1,5 @@
-import React, { useRef, useEffect } from "react";
-import { Link } from "react-router-dom";
+import React, { useRef } from "react";
+import axios from "axios";
 import Webcam from "react-webcam";
 import Botao from "../form/Botao";
 import BotaoBranco from "../form/BotaoBranco";
@@ -8,22 +8,26 @@ import styles from "./Registro.module.css";
 function RegistroFotografico() {
   const webcamRef = useRef(null);
 
-  const capture = () => {
+  const capture = async () => {
     if (webcamRef.current) {
       const imageSrc = webcamRef.current.getScreenshot();
-      console.log(imageSrc);
-    }
-  };
+      const captureTime = new Date().toISOString(); // Obter o horário atual como string no formato ISO
 
-  useEffect(() => {
-    checkCameraPermissions();
-  }, []);
+      // Enviar a imagem e o horário para o backend
+      try {
+        const response = await axios.post(
+          "http://127.0.0.1:8000/api/cargahorariafuncionario",
+          {
+            tb_funcionario_cd_funcionario: 13,
+            base64Image: imageSrc,
+            captureTime: captureTime,
+          }
+        );
 
-  const checkCameraPermissions = async () => {
-    try {
-      await navigator.mediaDevices.getUserMedia({ video: true });
-    } catch (error) {
-      console.error("Erro ao acessar a câmera:", error);
+        console.log("Resposta do servidor:", response.data);
+      } catch (error) {
+        console.error("Erro ao enviar imagem para o servidor:", error);
+      }
     }
   };
 
@@ -33,9 +37,7 @@ function RegistroFotografico() {
         <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
       </div>
       <div className={styles.divbtn}>
-        <Link to="/selecionarfuncionario">
-          <BotaoBranco text="Voltar" />
-        </Link>
+        <BotaoBranco text="Voltar" />
         <Botao onClick={capture} text="Registrar" />
       </div>
     </div>
